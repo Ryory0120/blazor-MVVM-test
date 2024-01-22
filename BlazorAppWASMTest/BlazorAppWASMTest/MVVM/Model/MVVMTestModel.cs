075589ad92de788761198cb42798007c6b1166e1
Ryory0120 <155705295+Ryory0120@users.noreply.github.com>
2024-01-23 08:40:35 +0900

    First Commit

---
+++ BlazorAppWASMTest/BlazorAppWASMTest/MVVM/Model/MVVMTestModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using System.Net.Http.Headers;
+
+namespace BlazorAppWASMTest.MVVM.Model
+{
+    public class MVVMTestModel
+    {
+        public string PCName { get; set; }
+
+        public MVVMTestModel()
+        {
+
+        }
+
+        public async Task InitializeAsync()
+        {
+            Console.WriteLine("MVVMTestModel InitializeAsync Start.");
+            
+            // 何かしらのサービスを利用してデータを初期化する。
+            await Task.Delay(1000);
+
+            this.PCName = "InitPCName";
+
+            Console.WriteLine("MVVMTestModel InitializeAsync Done.");
+            return;
+
+        }
+    }
+}
---
+++ BlazorAppWASMTest/BlazorAppWASMTest/MVVM/ViewModel/MVVMTestViewModel.cs
@@ -0,0 +1,33 @@
+﻿using BlazorAppWASMTest.MVVM.Model;
+
+namespace BlazorAppWASMTest.MVVM.ViewModel
+{
+    public class MVVMTestViewModel
+    {
+        public string PCName { get; set; } = String.Empty;
+
+        private MVVMTestModel mVVMTestModel;
+
+        public MVVMTestViewModel()
+        {
+            this.mVVMTestModel = new MVVMTestModel();
+        }
+
+        public async Task InitializeAsync()
+        {
+            Console.WriteLine("MVVMTestViewModel InitializeAsync Start.");
+            await Task.Delay(1000);
+            // Modelの初期化
+            await this.mVVMTestModel.InitializeAsync();
+
+            Console.WriteLine("MVVMTestViewModel InitializeAsync Done.");
+
+            // Modelから受け取ったデータを加工
+            Console.WriteLine("Modelから受け取ったデータの加工開始");
+            this.PCName = this.mVVMTestModel.PCName;
+            Console.WriteLine("Modelから受け取ったデータの加工終了");
+
+            return;
+        }
+    }
+}
